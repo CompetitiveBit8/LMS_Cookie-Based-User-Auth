b93855a0739b222531967e982d430fd48579e2c6
CompetitiveBit8 <babatundeksamuel@gmail.com>
2026-05-15 13:59:31 +0100

    User authorisation and authentication set up

--- Models/coursesModel.js
+++ Models/coursesModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const coursesSchema = new mongoose.Schema({
     title: {type: String, required: true},
-    author: {type: String, required: true},
+    author: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     published: {type: String, enum: [true, false], default: false, required: true},
     lengthInMinutes: {type: Number, required: true},
     }, 
